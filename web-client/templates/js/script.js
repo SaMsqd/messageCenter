21d1df557392da039ba82194015b7beeb8557bb1
Danilzone <Anton990.bel@gmail.com>
2024-02-21 19:34:02 +0300

    сделан sidebar

--- web-client/templates/js/script.js
+++ web-client/templates/js/script.js
@@ -1,1 +1,14 @@
 console.log("WEBPICKER")
+
+document.addEventListener('DOMContentLoaded', function(){
+    let sidebar = document.querySelector('.sidebar');
+    let chat = sidebar.querySelector('.chat');
+    let nochat = document.querySelector('.nochat');
+
+    if(chat === null) {
+        nochat.style.display = 'flex';
+    } else {
+        nochat.style.display = 'none';
+    }
+
+});
