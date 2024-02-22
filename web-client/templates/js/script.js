5edf8c3afe934cbc68368dc0fadb3dde06c16113
Danilzone <Anton990.bel@gmail.com>
2024-02-22 15:33:43 +0300

    В процессе

--- web-client/templates/js/script.js
+++ web-client/templates/js/script.js
@@ -1,14 +1,40 @@
-console.log("WEBPICKER")
+console.log("WEBPICKER");
 
 document.addEventListener('DOMContentLoaded', function(){
-    let sidebar = document.querySelector('.sidebar');
-    let chat = sidebar.querySelector('.chat');
-    let nochat = document.querySelector('.nochat');
+    let sidebar = document.querySelector('.sidebar');       // поиск левого меню    
+    let chatElements = sidebar.querySelectorAll('.chat');   // поиск всех .chat
+    let nochat = document.querySelector('.nochat');         // поиск блока ошибки
+    let mainChatForm = document.querySelector('.main')      // поиск формы чата
+    let lastTappedChat = null;                              // хранит ссылку на последний чат, на который было нажато
+    
+ 
+    chatElements.forEach(chat => {
 
-    if(chat === null) {
-        nochat.style.display = 'flex';
-    } else {
-        nochat.style.display = 'none';
-    }
+        // Если не найдет .chat, то сообщит об этом
+        if(chat === null) {
+            nochat.style.display = 'flex'; // если нашел .chat, то ошибка скрыта
+        } else {
+            nochat.style.display = 'none'; // если не нашел .chat, то покажет ошибку 
+        }
 
+        let checkCircle = chat.querySelector('.new-msg');
+        chat.onclick = tapFunc;
+
+        function tapFunc() {
+            if (lastTappedChat !== null) {              
+                lastTappedChat.style.borderColor = '';  // У предыдущего .chat 
+                lastTappedChat.style.borderWidth = '';  // пропадает цвет обводки и сама обводка
+            }
+            console.log("Tap!")
+            checkCircle.classList.add("__none");        // пропадет кружок
+            chat.style.borderColor = 'greenyellow';     // Обводка станет зеленой у текущего чата
+            chat.style.borderWidth = '2px';             // Обводка 2px
+            mainChatForm.style.display = 'flex';        // Появляется чат
+
+            
+            lastTappedChat = chat;                      // обновляем последний чат
+            
+
+        }
+    });
 });
