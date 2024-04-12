9fd722a3bccba44f2b69ba5095b97f328c2f218c
SaMsqd <arh1pov.sam@yandex.ru>
2024-04-12 20:05:08 +0300

    webhook fix

--- web_client/app.py
+++ web_client/app.py
@@ -5,7 +5,7 @@ from functools import wraps
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
-socketIO = SocketIO(app)
+socketIO = SocketIO(app, cors_allowed_origins='*', async_mode='threading')
 
 
 """
@@ -65,4 +65,4 @@ def new_message():
 
 
 if __name__ == '__main__':
-    socketIO.run(app)
+    socketIO.run(app, log_output=True, allow_unsafe_werkzeug=True)
