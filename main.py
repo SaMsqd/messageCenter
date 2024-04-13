a042849f070a6dc35b45d7575bdde44e55a00f75
SaMsqd <arh1pov.sam@yandex.ru>
2024-04-13 16:57:13 +0300

    flask -> fastapi websocket - fastapi.WebSocket

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-from avito.avito_api import AvitoApi
-import requests
+import asyncio
+from web import app, Websocket
+import threading
 
 
-client_id = '1GQx2lP_yfl6VT8RocTg'
+def start_server():
+    asyncio.run(app.main())
 
-client_secret = 'tLUAc-vrNV7rvdlS8WOmK5wJGlAJem5vCFG8hdqr'
-profile_id = 234428796
 
-api = AvitoApi(profile_id=profile_id, client_id=client_id, client_secret=client_secret)
-chat_id = api.chats_queue[1]['id']
-messages = api.get_chat(chat_id)
+def open_ws():
+    asyncio.run(websocket.main())
 
-for message in messages['messages']:
-    if message['content']['text'] == 'Хай':
-        api.send_message(chat_id=chat_id, text='Проверка API 2')
+def main():
+    thread_server = threading.Thread(target=start_server)
+    thread_ws = threading.Thread(target=open_ws)
 
+    thread_server.start()
+    thread_ws.start()
+
+
+if __name__ == '__main__':
+    main()
---
+++ web/app.py
@@ -0,0 +1,76 @@
+import fastapi
+from fastapi import WebSocket
+import uvicorn
+
+
+app = fastapi.FastAPI()
+
+
+class Manager:
+    def __init__(self):
+        self.active_connections = []
+
+    async def connect(self, ws: WebSocket):
+        await ws.accept()
+        self.active_connections.append(ws)
+
+    async def disconnect(self, ws: WebSocket):
+        self.active_connections.remove(ws)
+
+    async def send_message(self, ws: WebSocket, message):
+        await ws.send_text(message)
+
+    async def broadcast(self, message):
+        for con in self.active_connections:
+            await con.send_text(message)
+
+# def check_cookies(f):
+#     """
+#         Декоратор, который проверяет актуальность пароля пользователя, которые хранится в cookie
+#         В случае его неактуальности, перенаправвляет на страницу login
+#     """
+#     @wraps(f)
+#     def decorated_func(*args, **kwargs):
+#         if request.cookies.get('password') == get_password():
+#             return f(*args, **kwargs)
+#         else:
+#             return redirect(url_for('login'))
+#     return decorated_func
+
+
+manager = Manager()
+
+
+def return_html(file_name):
+    with open(f'./templates/{file_name}') as f:
+        return f.read()
+
+
+async def get_password():
+    """
+    Функция, которая будет получать пароль из файла с ботом и возвращать его
+    :return:
+    """
+    return 'test'
+
+
+@app.get('/login', response_class=fastapi.responses.HTMLResponse)
+@app.post('/login', response_class=fastapi.responses.HTMLResponse)
+async def login():
+    return return_html('index.html')
+
+@app.websocket('/')
+async def web_socket(ws: WebSocket):
+    await manager.connect(ws)
+    while True:
+        data = await ws.receive_text()
+        await manager.broadcast(data)
+
+
+@app.get('/chats')
+#@check_cookies
+def chats():
+    return 'Страница с чатами'
+
+
+uvicorn.run(app, port=5000, host='127.0.0.1')
