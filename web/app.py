5e2a11ac687dc6ba84b8242bc5647584f731bbc0
Danilzone <Anton990.bel@gmail.com>
2024-04-17 20:19:28 +0300

    почистил

--- web/app.py
+++ web/app.py
@@ -94,11 +94,18 @@ async def web_socket(ws: WebSocket):
 @app.websocket('/endless_ws')
 async def endless_ws(ws: WebSocket):
     await ws.accept()
-    while True:
-        await ws.send('Some data From WS')
-        time.sleep(2)
+    await ws.send_text('Some data From WS 1 ')
+            
+    for i in range(2):
+        if i == 0:
+            print("Отправленно сообщение")
+            await ws.send_text('Some data From WS 2')
+            i+=1
+        elif i == 1:
+            print("пауза 10 сек")
+            time.sleep(5)
 
-
+    # await ws.close()
 
 @app.get('/chats', response_class=fastapi.responses.HTMLResponse)
 @app.post('/chats', response_class=fastapi.responses.HTMLResponse)
