868dc3b2e0b1e7d78a4430ba10e299a2c94f3dd8
SaM-laptop <arh1pov.sam@yandex.ru>
2024-05-09 13:45:09 +0300

    /ws endpoint ECHO

--- app/routers/avito_webhook.py
+++ app/routers/avito_webhook.py
@@ -28,3 +28,11 @@ async def connect_websocket(ws: WebSocket):
 async def connect_websocket(ws: WebSocket):
     await ws.accept()
     ws_manager.connect(ws)
+
+
+@router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
