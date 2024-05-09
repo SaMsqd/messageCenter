254b29aea9d7bee7bc142cefd9b93c47d6554352
SaM-laptop <arh1pov.sam@yandex.ru>
2024-05-09 16:44:27 +0300

    /avito_webhook/ws

--- app/routers/avito_webhook.py
+++ app/routers/avito_webhook.py
@@ -1,9 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, WebSocket
 
 from app.websocket import WS_Manager
 
 
 router = APIRouter()
+
+
+@router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
 
 
 ws_manager = WS_Manager()
